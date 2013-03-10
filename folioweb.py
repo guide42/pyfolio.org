3ea02964440e14dcdbf842799dd6ce2f2bced2aa
Juan M <jm@guide42.com>
2013-03-10 22:06:16 +0100

    Add VERSION and bump it.

--- folioweb.py
+++ folioweb.py
@@ -7,6 +7,10 @@ from folio import Folio
 proj = Folio(__name__, extensions=[],
              jinja_extensions=['jinja2_highlight.HighlightExtension'])
 
+@proj.context('*.html')
+def version(env):
+    return {'version': '0.2'}
+
 def debug():
     import logging
 
